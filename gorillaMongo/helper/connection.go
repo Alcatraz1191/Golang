550fa37a912dcd641e2ca4e80a16b17826b44727
ShreyGadikar <shrygdkr@gmail.com>
2021-05-21 13:24:33 -0500

    gorillaMux with mondoDB

---
+++ gorillaMongo/helper/connection.go
@@ -0,0 +1,56 @@
+package helper
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//connectDB: Lets you connect to mongoDB
+//start with uppercase to see function when you import on other class
+func ConnectDB() *mongo.Collection{
+
+	//set client options
+	ClientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017/?readPreference=primary&appname=mongodb-vscode%200.5.0&ssl=false")
+
+	client, err := mongo.Connect(context.TODO(), ClientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("go_rest_api").Collection("books")
+
+	return collection
+}
+
+//ErrorResponse error model
+
+type ErrorResponse struct {
+	StatusCode int	`json:"status"`
+	ErrorMessage string	`json:"message"`
+}
+
+//GetError helper function for error model
+//start with capital letter
+func GetError(err error, w http.ResponseWriter) {
+
+	log.Fatal(err.Error())
+	var response = ErrorResponse {
+		ErrorMessage: err.Error(),
+		StatusCode: http.StatusInternalServerError,
+	}
+
+	message, _ := json.Marshal(response)
+
+	w.WriteHeader(response.StatusCode)
+	w.Write(message)
+}
+
---
+++ gorillaMongo/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"gorillaMongo/helper"
+	"gorillaMongo/models"
+)
+
+//Connection mongoDB with helper class
+var collection = helper.ConnectDB()
+func main(){
+	//Init Router
+	r := mux.NewRouter()
+
+	//arrange route
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	//r.HandleFunc("/api/books/{id]", updateBook).Method("GET")
+	//r.HandleFunc("/api/books/{id}", deleteBook).Method("GET")
+
+	//set port
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	//set header
+	w.Header().Set("Content-Type", "application/json")
+
+	//create a book array
+	var books []models.Book
+
+	//bson.M{}, pass empty filter to get all data
+	cur, err := collection.Find(context.TODO(), bson.M{})
+
+	if err != nil {
+		helper.GetError(err, w)
+	}
+	// defer closing cur
+	defer cur.Close(context.TODO())
+
+	for cur.Next(context.TODO()) {
+
+		// create variable to decode document into
+		var book models.Book
+		err := cur.Decode(&book)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//add item to our array
+		books = append(books, book)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(books)
+
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	//set header
+	w.Header().Set("Content-Type", "application/json")
+
+	var book models.Book
+
+	//get params with mux
+	var params = mux.Vars(r)
+
+	//string to ObjectID
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+
+	//create filter
+	filter := bson.M{"_id":id}
+
+	err := collection.FindOne(context.TODO(), filter).Decode(&book)
+
+	if err != nil {
+		helper.GetError(err, w)
+		return
+	}
+	json.NewEncoder(w).Encode(book)
+
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var book models.Book
+
+	//decode body
+	_= json.NewDecoder(r.Body).Decode(&book)
+
+	//insert our book model
+	result, err := collection.InsertOne(context.TODO(), book)
+
+	if err != nil {
+		helper.GetError(err, w)
+		return
+	}
+
+	json.NewEncoder(w).Encode(result)
+}
