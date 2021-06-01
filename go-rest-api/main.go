d2cdf1e262d40f4aa77c724f083e454741147de8
ShreyGadikar <shrygdkr@gmail.com>
2021-06-01 12:04:58 -0500

    RestAPI

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 )
 
-func main(){
-	router := mux.NewRouter()
-
-	router.HandleFunc("/home", home)
-
-	http.ListenAndServe(":8080", router)
-
+type Article struct {
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
 }
 
-func home(resp http.ResponseWriter, req *http.Request)  {
-	fmt.Println("Request landed in home..!")
-	fmt.Fprintf(resp, "Welcome to the HomePage!")
+var Articles []Article
+
+func main(){
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
+
+func homepage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the homepage!")
+	fmt.Println("Endpoint hit: Homepage")
+}
+
+func handleRequests(){
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
 }
---
+++ interviewQ1/go.mod
@@ -0,0 +1,3 @@
+module interviewq1
+
+go 1.16
