5dd826377bb93bed007f2fd5244d4919b87df920
ShreyGadikar <shrygdkr@gmail.com>
2021-06-16 14:25:16 -0500

    Initial commit

---
+++ SmallestOunce/main.go
@@ -0,0 +1,33 @@
+//Write a function that takes in a number of ounces and returns the smallest number of bars
+//of soap requred to fulfill the order. There are multiple sizes of soap bars: 2z, 5oz, 10oz
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	s := []int{2, 5, 10}
+	b := minBars(4, s)
+	fmt.Println(b)
+}
+
+func minBars(ounces int, s []int) int {
+	c := len(s) - 1
+	barCount := 0
+	sort.Ints(s)
+	for i := 0; ounces > 0; i++ {
+		if ounces >= s[c] {
+			ounces = ounces - s[c]
+			barCount++
+		} else {
+			c--
+		}
+		if c < 0 {
+			break
+		}
+	}
+	return barCount
+}
