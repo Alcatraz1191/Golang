29f526329aa87743a4c5a97eaaaca55152771396
ShreyGadikar <shrygdkr@gmail.com>
2021-07-16 15:48:55 -0500

    jenkinsfile

--- SocketServer/server.go
+++ SocketServer/server.go
@@ -9,36 +9,67 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+type GetSymbolJSON struct {
+	AvgProcessingTime  string `json:"avgProcessingTime"`
+	Crypto             bool   `json:"crypto"`
+	Delisted           bool   `json:"delisted"`
+	FullName           string `json:"fullName"`
+	HighProcessingTime string `json:"highProcessingTime"`
+	ID                 string `json:"id"`
+	LowProcessingTime  string `json:"lowProcessingTime"`
+	PayinConfirmations int64  `json:"payinConfirmations"`
+	PayinEnabled       bool   `json:"payinEnabled"`
+	PayinPaymentID     bool   `json:"payinPaymentId"`
+	PayoutEnabled      bool   `json:"payoutEnabled"`
+	PayoutFee          string `json:"payoutFee"`
+	PayoutIsPaymentID  bool   `json:"payoutIsPaymentId"`
+	PrecisionPayout    int64  `json:"precisionPayout"`
+	PrecisionTransfer  int64  `json:"precisionTransfer"`
+	TransferEnabled    bool   `json:"transferEnabled"`
+}
+
+
 var v interface{}
 
-func getCrypto(w http.ResponseWriter, r *http.Request){
+var SymbolJSON GetSymbolJSON
+
+func getCrypto(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	for{
+	for {
 		v = connectAPI("wss://api.hitbtc.com/api/2/ws/", "getCurrencies", "")
-		time.Sleep(time.Second*5)
+		time.Sleep(time.Second * 5)
 	}
 	json.NewEncoder(w).Encode(v)
 }
 
-func getSymbols(w http.ResponseWriter, r *http.Request){
+func getSymbols(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	for {
 		v = connectAPI("wss://api.hitbtc.com/api/2/ws/", "getSymbols", "")
-		time.Sleep(time.Second*5)
+		time.Sleep(time.Second * 5)
 	}
 	json.NewEncoder(w).Encode(v)
 }
 
-func getTrades(w http.ResponseWriter, r *http.Request){
+func getTrades(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "applicaiton/json")
 	for {
 		v = connectAPI("wss://api.hitbtc.com/api/2/ws/", "getTrades", "ETHBTC")
-		time.Sleep(time.Second*5)
+		time.Sleep(time.Second * 5)
 	}
 	json.NewEncoder(w).Encode(v)
 }
-func main() {
 
+func getSymbol(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "applicaiton/json")
+
+	v, _ = http.Get("http://api.hitbtc.com/api/2/public/currency")
+
+
+	json.NewEncoder(w).Encode(SymbolJSON)
+}
+func main() {
 
 	r := mux.NewRouter()
 
